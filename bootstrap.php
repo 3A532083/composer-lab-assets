e105bd82d4928b3d6e5db3d85cbcfb0e4c86be25
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-28 00:44:07 +0800

    carbon

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 33060);
 define('DB_DATABASE', '');
 define('DB_CHARSET', 'utf8');
-define('DB_USERNAME'root '');
-define('DB_PASSWORD'root '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
